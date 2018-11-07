21b8f96bd32ec4852cd757be4e625b362783fa2a
asadulm <mshaniasadulhaq@gmail.com>
2018-11-07 12:13:44 +0700

    fix style ambil_kuliah

--- AttendanceWebService/resources/views/ambil_kuliah/index.blade.php
+++ AttendanceWebService/resources/views/ambil_kuliah/index.blade.php
@@ -1,3 +1,28 @@
+<style>
+#list {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#list td, #list th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#list tr:nth-child(even){background-color: #d6d6db;}
+
+#list tr:hover {background-color: #66b3ff;}
+
+#list th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #3385ff;
+    color: white;
+}
+</style>
+
 @extends('layouts.main')
 
 @section('title')
@@ -6,7 +31,7 @@
 
 @section('contents')
 
-<table>
+<table id="list">
 	<tr>
 		<th>No.</th>
 		<th>Mata Kuliah</th>
@@ -26,7 +51,7 @@
 			{!! Form::open(['action' => 'AmbilKuliahController@peserta', 'method'=>'POST', 'class'=>'form-horizontal']) !!}
 			<input type="hidden" name="nama_mk" value="{{$p->nama_mk}}">
 			<input type="hidden" name="jadwal" value="{{$p->id}}">	
-			{!! Form::submit('Peserta') !!}
+			<center>{!! Form::submit('Peserta',['class' => 'btn btn-primary']) !!}</center>
 			{!! Form::close() !!}
 		</td>
 	</tr>
--- AttendanceWebService/resources/views/ambil_kuliah/peserta_index.blade.php
+++ AttendanceWebService/resources/views/ambil_kuliah/peserta_index.blade.php
@@ -1,5 +1,36 @@
-Tabel Peserta Kelas {{$nama_mk}}
-<table>
+<style>
+#list {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#list td, #list th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#list tr:nth-child(even){background-color: #d6d6db;}
+
+#list th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #3385ff;
+    color: white;
+}
+</style>
+
+@extends('layouts.main')
+
+@section('title')
+    Ambil Kuliah
+@endsection
+
+@section('contents')
+
+<strong style="font-size: 14pt">Tabel Peserta Kelas {{$nama_mk}}</strong>
+<table id='list'>
 	<tr>
 		<th>No.</th>
 		<th>NRP</th>
@@ -16,18 +47,21 @@ Tabel Peserta Kelas {{$nama_mk}}
 		<td>
 			{!! Form::open(['action' => 'AmbilKuliahController@delete', 'method'=>'POST', 'class'=>'form-horizontal']) !!}
 			<input type="hidden" name="id" value="{{$p->id}}">
-			{!! Form::submit('Hapus') !!}
+			{!! Form::submit('Hapus',['class' => 'btn btn-danger']) !!}
 			{!! Form::close() !!}
 		</td>
 	</tr>
 	@endforeach
 </table>
 
-Tambah Siswa
-{!! Form::open(['action' => 'AmbilKuliahController@angkatan', 'method'=>'POST', 'class'=>'form-horizontal']) !!}
-{!! Form::label('angkatan', 'Angkatan') !!}
-{!! Form::select('angkatan', [2013 => 2013, 2014 => 2014, 2015 => 2015, 2016 => 2016, 2017 => 2017, 2018 => 2018, 2019 => 2019])!!}
-<input type="hidden" name="jadwal" value="{{$jadwal}}"> 
-{!! Form::submit('oke') !!}
-{!! Form::close() !!}
+<div style="padding: 40px 0 0 0">
+	<strong style="font-size: 14pt">Tambah Siswa</strong>
+	{!! Form::open(['action' => 'AmbilKuliahController@angkatan', 'method'=>'POST', 'class'=>'form-horizontal']) !!}
+	{!! Form::label('angkatan', 'Angkatan', ['style' => 'font-size:12pt']) !!}
+	{!! Form::select('angkatan', [2013 => 2013, 2014 => 2014, 2015 => 2015, 2016 => 2016, 2017 => 2017, 2018 => 2018, 2019 => 2019])!!}
+	<input type="hidden" name="jadwal" value="{{$jadwal}}"> 
+	{!! Form::submit('Pilih',['class' => 'btn btn-primary']) !!}
+	{!! Form::close() !!}
+</div>
 
+@endsection
